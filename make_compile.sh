e3481e459df5c56a1d1590c50d4163c45b0c0636
mgimferrer <mgimferrer18@gmail.com>
2023-11-17 17:36:45 +0100

    Cleaning automatic PGO compilation

--- make_compile.sh
+++ make_compile.sh
@@ -1,13 +1,18 @@
 #!/bin/bash -l
 
-# THIS SCRIPT RUN SOME TESTING CALCULATIONS AS FIRST STEP OF THE PGO #
+## EXECUTING THIS SCRIPT ONE WILL COMPILE THE APOST3D CODE IN HIGH-PERFORMANCE MODE ##
+## THIS INVOLVES TWO STEPS: 1) GENERATION OF THE .dyn FILES (PROFILING) AND 2) USE  ##
+## OF THEM IN ORDER TO COMPILE WITH PROFILE GUIDED OPTIMIZATION (PGO)               ##
 
 PROGDIR="/home/mgimferrer/APOST3D"
 APOST="${PROGDIR}/apost3d"
 
+## STEP 1 ##
 make -f Makefile_profgen clean
 make -f Makefile_profgen
 ./compiler-runtest
+
+## STEP 2 ##
 make -f Makefile_profuse clean
 make -f Makefile_profuse
 ./compiler-runtest2
