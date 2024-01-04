5b2460d16e0bb859576963fabc82be046af9c22d
mgimferrer <mgimferrer18@gmail.com>
2024-01-04 17:01:04 +0100

    Automatic compilation of libxc

---
+++ compile_libxc.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+
+## EXECUTING THIS SCRIPT ONE WILL COMPILE THE LIBXC LIBRARIES REQUIRED           ##
+## WITH THE FLAGS, AND COPY OF FILES REQUIRED FOR LINKING AFTERWARDS TO APOST-3D ##
+
+PROG="/home/mgimferrer/APOST3D"
+LIBXCDIR="${PROG}/libxc-4.2.3"
+
+tar -xzvf libxc-4.2.3.tar.gz
+cd $LIBXCDIR
+
+## EXPORTS REQUIRED FOR CREATING PROPERLY THE MAKEFILES ##
+export CC=icc
+export FC=ifort
+export FCFLAGS="-u -fpp1 -nbs -pc80 -pad -align -unroll-aggressive -O3 -ip -no-fp-port -mno-ieee-fp -vec-report0 -no-prec-div -parallel -qopenmp"
+export LDFLAGS="-qopenmp"
+export CFLAGS="-O3"
+
+## CREATING MAKEFILES AND COMPILING ##
+./configure --prefix="${LIBXCDIR}"
+make
+make install
+
+## COPYING F90 INTERFACES REQUIRED TO LIBXCDIR FOLDER ##
+cp ./src/libxc_funcs.f90 . 
+cp ./src/libxc.f90 .
